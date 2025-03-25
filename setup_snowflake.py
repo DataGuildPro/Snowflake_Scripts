8e1d3efaca5b093ad75b8f28d061e55074365d08
yael hamo <yael@dataguild.pro>
2025-03-25 17:22:47 +0200

    updated raw prod schemas to get explicit privileges

--- setup_snowflake.py
+++ setup_snowflake.py
@@ -2,7 +2,7 @@ import random
 import string
 
 """
-This code creates a sql template file (structure_sql_output.sql) that you can run in order to setup snowflake initial structure.
+This code creates a sql template file (structure_sql_output.sql) that you can run in order to setup snowflakes initial structure.
 The placeholder lists hold names for- databases, schemas, warehouses and roles to create. 
 
 Some specific setups are defined under structure_sql_template.sql:
@@ -15,7 +15,9 @@ Some specific setups are defined under structure_sql_template.sql:
 """
 # Placeholder lists
 databases = ["raw_prod", "analytics_prod", "analytics_dev","analytics_stg"]
-schemas = ["honest_fulphilment",
+#schemas that appear here will get raw_prod_read access and raw_prod_write access.
+# If there is a schema that shouldn't get these privileges omit it from the following list and add its specificaly to - structure_sql_template.sql
+raw_prod_schemas = ["honest_fulphilment",
     "mysql", "stripe", "paypal", "shopify", "impact", "payoneer", "mongo", "mixpanel",
     "google_ads", "meta", "bing", "tiktok", "elastic_search", "ga4", "hubspot",
     "priority", "jira", "chargeflow", "connecteam", "userpilot", "trustpilot",
@@ -39,7 +41,7 @@ def generate_random_password(length=16):
 # Generate database creation statements
 database_creation_statements = "\n".join([f"CREATE DATABASE IF NOT EXISTS {database};" for database in databases])
 # Generate schema creation statements
-schema_creation_statements = "\n".join([f"CREATE SCHEMA IF NOT EXISTS {schema};" for schema in schemas])
+schema_creation_statements = "\n".join([f"CREATE SCHEMA IF NOT EXISTS {schema};" for schema in raw_prod_schemas])
 
 # Generate warehouse creation statements
 warehouse_creation_statements = "\n".join(
@@ -54,8 +56,7 @@ grant_roles = [("raw_prod","raw_prod_write"),("raw_prod","raw_prod_read")
 ("analytics_stg","analytics_stg_stg_read"),("analytics_stg","analytics_stg_stg_write"),
 ("analytics_dev","analytics_dev_read"),("analytics_dev","analytics_dev_write")]
 
-grant_write_roles = [("raw_prod","raw_prod_write")
-,("analytics_prod","analytics_prod_dwh_write"),
+grant_write_roles = [("analytics_prod","analytics_prod_dwh_write"),
 ("analytics_prod","analytics_prod_stg_write"),
 ("analytics_stg","analytics_stg_dwh_write"),
 ("analytics_stg","analytics_stg_stg_write"),
@@ -69,7 +70,7 @@ grant_read_roles = [("raw_prod","raw_prod_read")
 ("analytics_dev","analytics_dev_read")]
 
 
-grant_write_statements = ("GRANT CREATE SCHEMA, MONITOR, USAGE on database raw_prod to ROLE raw_prod_write;" +
+grant_write_statements = ("GRANT CREATE SCHEMA, MONITOR, USAGE on database raw_prod to ROLE raw_prod_write;\n" +
                             "\n".join([f"GRANT USAGE ON DATABASE {db} TO ROLE {role};\n"
                                        f"GRANT USAGE ON ALL SCHEMAS IN DATABASE {db} TO ROLE {role};\n"
                                         f"GRANT USAGE ON FUTURE SCHEMAS IN DATABASE {db} to ROLE {role};\n"    
@@ -81,6 +82,18 @@ grant_write_statements = ("GRANT CREATE SCHEMA, MONITOR, USAGE on database raw_p
                                         f"GRANT ALL ON FUTURE TABLES IN DATABASE {db} TO ROLE {role};\n"
                                         f"GRANT ALL ON FUTURE VIEWS IN DATABASE {db} TO ROLE {role};\n"
                                        for db, role in grant_write_roles]))
+# for raw_prod_write - we want to give the privileges to a specific list of schemas
+# this is because there are cases where a schema in this db needs restricted access, therefor we don't want to automatically give write privileges to all schemas.
+# if there is such a case please add the role and schema (creation and privileges) specifically and don't add it to the list - raw_prod_schemas .
+
+grant_raw_prod_write_statement = (f"GRANT USAGE ON FUTURE SCHEMAS IN DATABASE raw_prod to ROLE raw_prod_write;\n"
+                                  + "\n".join(
+                                [f"GRANT USAGE ON SCHEMA raw_prod.{schema} TO ROLE raw_prod_read;\n"
+                                 f"GRANT ALL PRIVILEGES ON TABLES IN SCHEMA raw_prod.{schema} TO ROLE raw_prod_write;\n"
+                                 f"GRANT ALL PRIVILEGES ON VIEWS IN SCHEMA raw_prod.{schema} TO ROLE raw_prod_write;\n"
+                                 f"GRANT ALL PRIVILEGES ON FUTURE TABLES IN SCHEMA raw_prod.{schema} TO ROLE raw_prod_write;\n"
+                                 f"GRANT ALL PRIVILEGES ON FUTURE VIEWS IN SCHEMA raw_prod.{schema} TO ROLE raw_prod_write;\n"
+                                 for schema in raw_prod_schemas]))
 
 grant_read_statements = (" \n".join([f"GRANT USAGE ON DATABASE {db} TO ROLE {role};\n"
                                         f"GRANT USAGE ON ALL SCHEMAS IN DATABASE {db} TO ROLE {role};\n"
@@ -129,7 +142,7 @@ if __name__ == "__main__":
         schema_creation=schema_creation_statements,
         warehouse_creation=warehouse_creation_statements,
         role_creation=role_creation_statements,
-        grants_statement=grant_read_statements + grant_write_statements,
+        grants_statement=grant_read_statements + grant_write_statements + grant_raw_prod_write_statement,
         grant_sysadmin_statements=grant_sysadmin_statements,
         **passwords
     )
